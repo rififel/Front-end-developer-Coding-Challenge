7d2cb8a5827a153b42c57b15fe947e164ee69abd
rififel <95085626+rififel@users.noreply.github.com>
2021-11-26 21:13:01 +0800

    Add files via upload

---
+++ Indonesian-Destination/assets/js/test1.js
@@ -0,0 +1,9 @@
+// Expected Result = 55
+// Direction : Return sum of array
+const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function result(input) {
+  // Your Code Here
+}
+
+console.log(result(input));
